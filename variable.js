40ab08d16a9e1982338c5229a237c9b08b72f937
shahidkhan121 <shah12pp@gmail.com>
2024-04-04 03:29:16 +0500

    .

--- variable.js
+++ variable.js
@@ -1,2 +1,25 @@
 accountCity="swat"
-console.log(accountCity);
+// console.log(accountCity);
+// in java script we can even use a varibale without defining its datatype but we shouldnot do that bcz not a good practice
+
+
+// ==========const keyword=======//
+const userId = 1234567;
+// userId=22222
+// console.log(userId)
+// this will not run bcz we cannot assign another value to same variable having a const keyword
+
+
+
+//========var and Let keyword=========
+var userName="shahid"
+let userPass="khan"
+
+
+console.table([userId,userName,userPass,accountCity])
+
+// console.table([]) : is used when we have differnt and more varibale and we want it to print them in single console line so we use it 
+
+
+
+
