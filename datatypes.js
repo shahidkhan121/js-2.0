6549b00c2d74a9b597643fbf79d0bc5cf050a514
shahidkhan121 <shah12pp@gmail.com>
2024-04-05 16:02:51 +0500

    .

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+// alert(3+3)   we are using node js not browser--->alert will run in browser while for node js it has different syntex 
+
+
+
+// "use strict"
+cityName= "swat";
+console.log(cityName);
+
+
+
+
+let pass =1;
+console.log(typeof pass)
+
+
+
+let work = true
+console.log(typeof work);
+
+
+let id =1.2
+console.log(typeof id);
+
+
+// console.log(x)
+// let x=10
+
+
+console.log(y)
+var y =10
+
